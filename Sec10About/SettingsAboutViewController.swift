ca5c83c23c62a84904cf7d899c07fe7f3a435b76
ToshiroSama <alishdjuraev@gmail.com>
2023-02-02 14:00:09 +0500

    вфвфв

--- Sec10About/SettingsAboutViewController.swift
+++ Sec10About/SettingsAboutViewController.swift
@@ -12,6 +12,43 @@ final class SettingsAboutView: UIView {
         return imageView
     }()
     
+    private let firstLabel: UILabel = {
+        let label = UILabel()
+        label.text = "My Uztelecom"
+        label.font = .systemFont(ofSize: 17, weight: .semibold)
+        return label
+    }()
+    
+    private let secondLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Версия 3.0.1"
+        label.textColor = UIColor(red: 170/255, green: 171/255, blue: 173/255, alpha: 1.0)
+        label.font = .systemFont(ofSize: 15, weight: .semibold)
+        return label
+    }()
+    
+    private let firstButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Публичная оферта компании", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 13, weight: .semibold)
+        button.backgroundColor = UIColor(red: 248/255, green: 248/255, blue: 248/255, alpha: 1.0)
+        button.layer.cornerRadius = 16
+        button.clipsToBounds = true
+        return button
+    }()
+    
+    private let secondButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Оферта платёжной платформы", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 13, weight: .semibold)
+        button.backgroundColor = UIColor(red: 248/255, green: 248/255, blue: 248/255, alpha: 1.0)
+        button.layer.cornerRadius = 16
+        button.clipsToBounds = true
+        return button
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -25,14 +62,42 @@ final class SettingsAboutView: UIView {
     
     private func addSubviews() {
         self.addSubview(logoImg)
+        self.addSubview(firstLabel)
+        self.addSubview(secondLabel)
+        self.addSubview(firstButton)
+        self.addSubview(secondButton)
     }
     
     private func setConstraints() {
         
         logoImg.snp.makeConstraints { make in
             make.top.equalTo(self.snp.top).offset(74)
-            make.leading.equalTo(self.snp.leading).offset(140)
+            make.centerX.equalTo(self.snp.centerX)
             make.size.equalTo(CGSize(width: 96, height: 96))
+        }
+        
+        firstLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.logoImg.snp.bottom).offset(24)
+            make.centerX.equalTo(self.snp.centerX)
+        }
+        
+        secondLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.firstLabel.snp.bottom).offset(8)
+            make.centerX.equalTo(self.snp.centerX)
+        }
+        
+        firstButton.snp.makeConstraints { make in
+            make.top.equalTo(self.secondLabel.snp.bottom).offset(64)
+            make.leading.equalTo(self.snp.leading).offset(24)
+            make.trailing.equalTo(self.snp.trailing).offset(-24)
+            make.height.equalTo(44)
+        }
+        
+        secondButton.snp.makeConstraints { make in
+            make.top.equalTo(self.firstButton.snp.bottom).offset(16)
+            make.leading.equalTo(self.firstButton.snp.leading)
+            make.trailing.equalTo(self.firstButton.snp.trailing)
+            make.height.equalTo(44)
         }
     }
     
