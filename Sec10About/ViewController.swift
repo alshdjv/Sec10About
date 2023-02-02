ac57a57ce30a67792b84861df11c5e625c385632
ToshiroSama <alishdjuraev@gmail.com>
2023-02-02 09:12:29 +0500

    Initial Commit

---
+++ Sec10About/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sec10About
+//
+//  Created by Alisher Djuraev on 02/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
